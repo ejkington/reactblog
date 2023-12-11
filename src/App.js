9edc9189975123631dc8bd286b19101dce041521
ejkington <fareborg@hotmail.com>
2023-12-11 22:20:04 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <div id="page-body">
+        Welcome to my blog!
+      </div>
+    </div>
+  );
+}
+
+export default App;
